8a1bf3d2481e8d898557b75dedea337e71c97eb2
galyaminavas <galyaminavas@gmail.com>
2020-03-03 15:56:19 +0300

    Third week assignment done

---
+++ ProgrammingAssignment3/Assets/Scripts/Rock.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A spawned rock
+/// </summary>
+public class Rock : MonoBehaviour
+{
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+        // apply impulse force to get game object moving
+        const float MinImpulseForce = 1f;
+        const float MaxImpulseForce = 2f;
+        float angle = Random.Range(0, 2 * Mathf.PI);
+        Vector2 direction = new Vector2(
+            Mathf.Cos(angle), Mathf.Sin(angle));
+        float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
+        GetComponent<Rigidbody2D>().AddForce(
+            direction * magnitude,
+            ForceMode2D.Impulse);
+    }
+
+    /// <summary>
+    /// Destroy rock when it leaves the screen
+    /// </summary>
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ ProgrammingAssignment3/Assets/Scripts/RockSpawner.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A rock spawner
+/// </summary>
+public class RockSpawner : MonoBehaviour
+{
+    // needed for spawning
+    [SerializeField]
+    GameObject prefabRock;
+
+    // saved for efficiency
+    [SerializeField]
+    Sprite rockSprite0;
+    [SerializeField]
+    Sprite rockSprite1;
+    [SerializeField]
+    Sprite rockSprite2;
+
+    // spawn control
+    const float SpawnDelay = 1;
+    Timer spawnTimer;
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start()
+    {
+        // create and start timer
+        spawnTimer = gameObject.AddComponent<Timer>();
+        spawnTimer.Duration = SpawnDelay;
+        spawnTimer.Run();
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        int rocksCount = GameObject.FindGameObjectsWithTag("Rock").Length;
+        // check for time to span a new rock and for current number of rocks on the screen
+        if (spawnTimer.Finished && rocksCount < 3)
+        {
+            SpawnRock();
+
+            // restart timer
+            spawnTimer.Run();
+        }
+    }
+
+    /// <summary>
+    /// Spawns a new rock at the center of the screen
+    /// </summary>
+    void SpawnRock()
+    {
+        GameObject rock = Instantiate(prefabRock) as GameObject;
+        // spawn at the center of the screen support
+        Vector3 location = new Vector3(Screen.width / 2, Screen.height / 2, -Camera.main.transform.position.z);
+        Vector3 worldLocation = Camera.main.ScreenToWorldPoint(location);
+        rock.transform.position = worldLocation;
+
+        // set random sprite for new rock
+        SpriteRenderer spriteRenderer = rock.GetComponent<SpriteRenderer>();
+        int spriteNumber = Random.Range(0, 3);
+        if (spriteNumber == 0)
+        {
+            spriteRenderer.sprite = rockSprite0;
+        } else if (spriteNumber == 1)
+        {
+            spriteRenderer.sprite = rockSprite1;
+        } else
+        {
+            spriteRenderer.sprite = rockSprite2;
+        }
+        //switch (spriteNumber)
+        //{
+        //    case 0:
+        //        spriteRenderer.sprite = rockSprite0;
+        //        break;
+        //    case 1:
+        //        spriteRenderer.sprite = rockSprite1;
+        //        break;
+        //    case 2:
+        //        spriteRenderer.sprite = rockSprite2;
+        //        break;
+        //}
+    }
+}
